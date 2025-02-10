a3bc0c41a8132fc138ea8670d70e7418d5214ca8
John Doe <johndoe@example.com>
2025-02-10 08:00:16 +0300

    start

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test
+
+go 1.23.5
+
+require github.com/go-chi/chi v1.5.5 // indirect
--- proxy/main.go
+++ proxy/main.go
@@ -2,34 +2,51 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-chi/chi"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(ProxyTest())
 
-	// ...
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	//go WorkerTest()
 
 	http.ListenAndServe(":8080", r)
 }
 
-const content = ``
+const content = `123`
 
 func WorkerTest() {
 	t := time.NewTicker(1 * time.Second)
+	defer t.Stop()
 	var b byte = 0
-	for {
-		select {
-		case <-t.C:
-			err := os.WriteFile("/app/static/_index.md", []byte(fmt.Sprintf(content, b)), 0644)
-			if err != nil {
-				log.Println(err)
-			}
-			b++
+	for range t.C {
+		err := os.WriteFile("/app/static/_index.md", []byte(fmt.Sprintf("%s %d", content, b)), 0644)
+		if err != nil {
+			log.Println(err)
 		}
+		b++
+	}
+}
+
+func ProxyTest() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			path := r.URL.Path
+			if len(path) >= 4 && path[1:4] == "api" {
+				w.Write([]byte("Hello from API"))
+			} else {
+				http.Redirect(w, r, "http://localhost:1313", http.StatusFound)
+			}
+		})
 	}
 }
